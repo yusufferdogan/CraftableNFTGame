45ee037e87ce3dee6b94d06f2bbdd82759c36e3e
Ahmet Yusuf Erdoğan <yusuferdogan@bixos.com>
2022-05-09 22:16:02 +0300

    add faucet method to token

--- contracts/FooToken.sol
+++ contracts/FooToken.sol
@@ -8,8 +8,16 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FooToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Foo Token", "FOO") {
+    mapping(address => uint256) withdrawalTimes;
+
+    constructor(uint256 initialSupply) ERC20("Craft Token", "CRAFT") {
         _mint(_msgSender(), initialSupply);
+    }
+
+    function faucet() public {
+        require(block.timestamp > withdrawalTimes[_msgSender()], "You can faucet per 5 minutes");
+        withdrawalTimes[_msgSender()] = block.timestamp + 5 minutes;
+        _mint(_msgSender(), 5);
     }
 
     function decimals() public pure override returns (uint8) {
